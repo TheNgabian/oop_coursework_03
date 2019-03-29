4da1fb7898292b08ffd29ffb46d63ecfe24f894a
TheNgabian <47911398+TheNgabian@users.noreply.github.com>
2019-03-29 18:48:32 +0300

    Add files via upload

---
+++ CourseWork-3/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student sd1 = new Student("18/123/BIT","Dov",1990);	
+		
+		System.out.println("STUDENT NAME :"+sd1.getName());
+		
+		System.out.println("STUDENT YEAR OF BIRTH :"+sd1.getYOB());		
+		
+		System.out.println("STUDENT REGISTRATION NUMBER :"+sd1.getRegNO());	
+		sd1.getCourseWork();
+		
+		
+		
+		
+	
+	
+	
+	
+}
+}
+
